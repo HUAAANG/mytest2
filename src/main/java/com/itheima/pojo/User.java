599d4f038e4cab99a73c19d32a8db14f792085ba
翔哥 <894549548@qq.com>
2018-11-08 21:42:20 +0800

    提交删除

--- src/main/java/com/itheima/pojo/User.java
+++
@@ -1,9 +0,0 @@
-package com.itheima.pojo;
-
-public class User {
-    private String name;
-    public static void main(String[] args) {
-        System.out.println("哈哈哈哈");
-        System.out.println("小明");
-    }
-}
